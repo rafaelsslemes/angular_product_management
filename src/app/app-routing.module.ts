5334e4c202d3f2132ec96554fdabe1e92fc73440
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-27 20:16:46 -0300

    Routes to navigation by pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { ProductManagementComponent } from './view/product-management/product-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent},
+  {
+    path: "products",
+    component: ProductManagementComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
