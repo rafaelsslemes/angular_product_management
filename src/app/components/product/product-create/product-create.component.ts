e3f06cf234c14f16610e14178803c7edfe4680f2
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-29 19:12:25 -0300

    CreateProduct implemented

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { Product } from '../product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -9,14 +11,29 @@ import { ProductService } from '../product.service';
 export class ProductCreateComponent implements OnInit {
 
   // Receiving in contructor invokes Angular to Inject an instance by DI
-  constructor(private productService : ProductService){}
+  constructor(private productService : ProductService, private router : Router){}
 
   ngOnInit(): void {
       
   }
 
+  product: Product = {
+    name: "Test Product",
+    price: 150
+  }
+  
   createProduct(){
-    this.productService.showMessage("Product created");
+
+    // Get an observable and run commands when it receives a conclusion event 
+    this.productService.create(this.product).subscribe(()=>{
+      
+      this.productService.showMessage("Product created");
+      this.router.navigate(["/products"]);
+    });
+  }
+
+  cancel(){
+    this.router.navigate(["/products"]);
   }
   
 }
