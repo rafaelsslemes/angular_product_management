0a76bf45a6c4cd88d99fb89c3744f96e48a4200b
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-31 19:15:27 -0300

    Error handling on create method

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { Product } from './product.model';
 
 @Injectable({
@@ -14,17 +14,28 @@ export class ProductService {
   // Receiving in contructor invokes Angular to Inject an instance by DI
   constructor(private snackBar : MatSnackBar, private http : HttpClient) { }
 
-  showMessage(msg : string){
+  showMessage(msg : string, isError : boolean = false){
     this.snackBar.open(msg, '', 
       {
         duration: 3000,
         horizontalPosition: 'right',
-        verticalPosition: 'top'
+        verticalPosition: 'top',
+        panelClass: isError ? ['msg-error'] : ['msg-success']
       });
   }
 
   create (product : Product) : Observable<Product>{
-    return this.http.post<Product>(this.backendUrl, product);
+    return this.http.post<Product>(this.backendUrl, product).pipe<Product, Product>
+    (
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e)
+    ));
+  }
+
+  errorHandler(e: any): Observable<never> {
+    console.log(e);
+    this.showMessage("An error ocurried", true);
+    return EMPTY;
   }
 
   read () : Observable<Product[]> {
