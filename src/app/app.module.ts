4aca84d50e4b9b9d0e25d5e916b2182056835f39
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-31 06:36:31 -0300

    Format currency and number separator to PT-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,6 +30,11 @@ import { ProductReadTableComponent } from './components/product/product-read-tab
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+
+import localtePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localtePt);
 
 @NgModule({
   declarations: [
@@ -63,7 +68,11 @@ import { MatSortModule } from '@angular/material/sort';
     MatPaginatorModule,
     MatSortModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'PT-BR'
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
