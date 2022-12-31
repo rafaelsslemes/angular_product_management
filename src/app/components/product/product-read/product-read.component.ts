fea5ee8072f46ce2ecdd67228997b4a1f36633af
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-31 06:51:57 -0300

    Column Actions

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -11,7 +11,7 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit{
 
   products! : Product[];
-  displayedColumns = ["id", "name", "price"];
+  displayedColumns = ["id", "name", "price", "actions"];
 
   constructor(private productService : ProductService){}
 
