d77f608d4dff64080539cb444047fc5ee9b1f3a0
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-31 07:52:57 -0300

    Update Component

--- src/app/components/product/produc-update/produc-update.component.ts
+++ src/app/components/product/produc-update/produc-update.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-produc-update',
   templateUrl: './produc-update.component.html',
   styleUrls: ['./produc-update.component.css']
 })
-export class ProducUpdateComponent {
+export class ProducUpdateComponent implements OnInit{
+  
+  product!: Product;
+
+  constructor (private productService : ProductService, 
+               private router : Router,
+               private route : ActivatedRoute){}
+
+  ngOnInit(): void {
+      const id = this.route.snapshot.paramMap.get("id");
+      this.productService.readById(id!).subscribe(product => {
+        this.product = product;
+      });
+  }
+
+  updateProduct(){
+    this.productService.update(this.product).subscribe(()=>{
+      this.productService.showMessage("Product Updated!");
+      this.router.navigate(["/products"]);
+    });
+  }
+  
+  cancel(){
+    this.router.navigate(["/products"]);
+  }
 
 }
